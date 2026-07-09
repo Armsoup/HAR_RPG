76f6d298ef444d48642d9cb48cb02c86bbfe5862
Armsoup <arslonsato@gmail.com>
2026-07-09 16:46:54 +0300

    Add files via upload

--- HAR.cpp
+++ HAR.cpp
@@ -37,22 +37,22 @@ int achievementsdist10000 = 0;
 int achievementsdist40075 = 0;
 
 void game() {
-	setColor("1f");
+	IO::setColor("1f");
 	if (Distancecovered == 0) {
 		// Backstory...
-		print_slow("-Hi! I found something...", 75);
+		IO::print_slow("-Hi! I found something...", 75);
 		Sleep(3500);
-		print_slow("-Hi, so what strange thing did you find?", 75);
+		IO::print_slow("-Hi, so what strange thing did you find?", 75);
 		Sleep(3500);
-		print_slow("-It... looks like some kind of creature!", 75);
+		IO::print_slow("-It... looks like some kind of creature!", 75);
 		Sleep(3500);
-		print_slow("-Hahaha, I don't believe you, so I'm heading into that forest myself!", 75);
-		pause();
-		clearScreen();
+		IO::print_slow("-Hahaha, I don't believe you, so I'm heading into that forest myself!", 75);
+		IO::pause();
+		IO::clearScreen();
 	}
-	if (PlayerFood > 40) { cout << "Cheater, huh?" << endl; pause(); exit(0); }
+	if (PlayerFood > 40) { cout << "Cheater, huh?" << endl; IO::pause(); exit(0); }
 	if (PlayerFood == 0 && PlayerFoodInventory == 0) {
-		setColor("4f");
+		IO::setColor("4f");
 		cout << "=========================================" << endl;
 		cout << "  YOU DIED OF HUNGER IN THE FOREST...   " << endl;
 		cout << "=========================================" << endl;
@@ -60,35 +60,35 @@ void game() {
 		cout << "Monsters killed: " << KillMonsters << "." << endl;
 		cout << "Food found: " << FoundFood << "." << endl;
 		cout << "Kilometers covered: " << Distancecovered * 0.5 << endl;
-		pause();
+		IO::pause();
 		string ForestMap3D =
-			"################"
-			"#..............#"
-			"#..###....###..#"
-			"#..#........#..#"
-			"#..#........#..#"
-			"#......##......#"
-			"#......##......#"
-			"#..............#"
-			"#..............#"
-			"#..............#"
-			"#..............#"
-			"#..............#"
-			"#..#........#..#"
-			"#..............#"
-			"#..............#"
-			"################";
+			"#..............."
+			"#..............."
+			"#..###....###..."
+			"#..#........#..."
+			"#..#........#..."
+			"#......##......."
+			"#......##......."
+			"................"
+			"#..............."
+			"#..............."
+			"#..............."
+			"#..............."
+			"#..#........#..."
+			"#..............."
+			"#..............."
+			"#...............";
 		float PlayerAngle3D = 0.0f;
 		PlayerX = 2.0f;
 		PlayerY = 2.0f;
 		PlayerFood = 0;
 		while (true) { // Endless 3D walking mode
-			if (isKeyPressed('W')) {
+			if (IO::isKeyPressed('W')) {
 				int hungry = 0;
 				if (hungry == 0) {
 					float nextX = PlayerX + sinf(PlayerAngle3D) * 0.08f;
 					float nextY = PlayerY + cosf(PlayerAngle3D) * 0.08f;
-					if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] != '#') {
+					if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '.' || ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '\0') {
 						PlayerX = nextX;
 						PlayerY = nextY;
 					}
@@ -96,22 +96,22 @@ void game() {
 				}
 			}
 
-			if (isKeyPressed('S')) {
+			if (IO::isKeyPressed('S')) {
 				int hungry = 0;
 				if (hungry == 0) {
 					float nextX = PlayerX - sinf(PlayerAngle3D) * 0.08f;
 					float nextY = PlayerY - cosf(PlayerAngle3D) * 0.08f;
-					if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] != '#') {
+					if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '.' || ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '\0') {
 						PlayerX = nextX;
 						PlayerY = nextY;
 					}
 					Sleep(40);
 				}
 			}
-			if (isKeyPressed('A')) PlayerAngle3D -= 0.05f;
-			if (isKeyPressed('D')) PlayerAngle3D += 0.05f;
-			if (isKeyPressed('P')) exit(0);
-			render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16);
+			if (IO::isKeyPressed('A')) PlayerAngle3D -= 0.05f;
+			if (IO::isKeyPressed('D')) PlayerAngle3D += 0.05f;
+			if (IO::isKeyPressed('P')) exit(0);
+			Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16);
 			Sleep(15);
 		}
 	}
@@ -124,7 +124,7 @@ void game() {
 		if (achievementskill1 == 0) {
 			achievementskill1 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: Kill the first monster!");
+			Filesystem::Achievements("UNLOCKED: Kill the first monster!");
 			cout << "ACHIEVEMENT UNLOCKED: Kill the first monster!" << endl;
 		}
 	}
@@ -132,7 +132,7 @@ void game() {
 		if (achievementskill10 == 0) {
 			achievementskill10 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: Kill ten monsters!");
+			Filesystem::Achievements("UNLOCKED: Kill ten monsters!");
 			cout << "ACHIEVEMENT UNLOCKED: Kill ten monsters!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 6;
 		}
@@ -141,7 +141,7 @@ void game() {
 		if (achievementskill100 == 0) {
 			achievementskill100 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: GOD of monsters! (Kill a hundred monsters!)");
+			Filesystem::Achievements("UNLOCKED: GOD of monsters! (Kill a hundred monsters!)");
 			cout << "ACHIEVEMENT UNLOCKED: GOD of monsters!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 60;
 		}
@@ -150,7 +150,7 @@ void game() {
 		if (achievementsfood1 == 0) {
 			achievementsfood1 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: Find the first food!");
+			Filesystem::Achievements("UNLOCKED: Find the first food!");
 			cout << "ACHIEVEMENT UNLOCKED: Find the first food!" << endl;
 		}
 	}
@@ -158,7 +158,7 @@ void game() {
 		if (achievementsfood10 == 0) {
 			achievementsfood10 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: Find ten cans of food!");
+			Filesystem::Achievements("UNLOCKED: Find ten cans of food!");
 			cout << "ACHIEVEMENT UNLOCKED: Find ten cans of food!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 4;
 		}
@@ -167,7 +167,7 @@ void game() {
 		if (achievementsfood100 == 0) {
 			achievementsfood100 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: GOD of meat and canned goods! (Find 100 cans of food!)");
+			Filesystem::Achievements("UNLOCKED: GOD of meat and canned goods! (Find 100 cans of food!)");
 			cout << "ACHIEVEMENT UNLOCKED: GOD of meat and canned goods!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 65;
 		}
@@ -176,7 +176,7 @@ void game() {
 		if (achievementsdist10 == 0) {
 			achievementsdist10 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: The first 10 kilometers!");
+			Filesystem::Achievements("UNLOCKED: The first 10 kilometers!");
 			cout << "ACHIEVEMENT UNLOCKED: The first 10 kilometers!" << endl;
 		}
 	}
@@ -184,7 +184,7 @@ void game() {
 		if (achievementsdist100 == 0) {
 			achievementsdist100 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: The first 100 kilometers!");
+			Filesystem::Achievements("UNLOCKED: The first 100 kilometers!");
 			cout << "ACHIEVEMENT UNLOCKED: The first 100 kilometers!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 6;
 		}
@@ -193,7 +193,7 @@ void game() {
 		if (achievementsdist1000 == 0) {
 			achievementsdist1000 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: The first 1000 kilometers!");
+			Filesystem::Achievements("UNLOCKED: The first 1000 kilometers!");
 			cout << "ACHIEVEMENT UNLOCKED: The first 1000 kilometers!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 30;
 		}
@@ -202,7 +202,7 @@ void game() {
 		if (achievementsdist10000 == 0) {
 			achievementsdist10000 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: The first 10000 kilometers!");
+			Filesystem::Achievements("UNLOCKED: The first 10000 kilometers!");
 			cout << "ACHIEVEMENT UNLOCKED: The first 10000 kilometers!" << endl;
 			PlayerFoodInventory = PlayerFoodInventory + 100;
 		}
@@ -211,7 +211,7 @@ void game() {
 		if (achievementsdist40075 == 0) {
 			achievementsdist40075 = 1;
 			cout << endl;
-			Achievements("UNLOCKED: A trip around the world! (Cover 40,075 km)");
+			Filesystem::Achievements("UNLOCKED: A trip around the world! (Cover 40,075 km)");
 			cout << "ACHIEVEMENT UNLOCKED: A trip around the world!" << endl;
 			PlayerFoodInventory = 200000;
 		}
@@ -222,7 +222,7 @@ void game() {
 		cout << "You found canned food! (+1 to inventory)" << endl;
 		FoundFood++;
 		GlobalEvent = 0;
-		saveGame();
+		SLgame::saveGame();
 	}
 	else if (GlobalEvent == 2) {
 		cout << "A fierce monster is in front of you! It blocks the way!" << endl;
@@ -251,12 +251,12 @@ void game() {
 	case 'w': {
 		if (GlobalEvent == 2) {
 			cout << "The monster won't let you forward! Defeat it first!" << endl;
-			pause();
+			IO::pause();
 			break;
 		}
 		if (PlayerFood == 0) {
 			cout << "You are hungry! Eat some food!" << endl;
-			pause();
+			IO::pause();
 			break;
 		}
 		PlayerY++;
@@ -274,8 +274,8 @@ void game() {
 		break;
 	}
 	case 'a': {
-		if (GlobalEvent == 2) { cout << "You cannot escape from the monster to the left!" << endl; pause(); break; }
-		if (PlayerFood == 0) { cout << "You are hungry!" << endl; pause(); break; }
+		if (GlobalEvent == 2) { cout << "You cannot escape from the monster to the left!" << endl; IO::pause(); break; }
+		if (PlayerFood == 0) { cout << "You are hungry!" << endl; IO::pause(); break; }
 		PlayerX--;
 		Distancecovered++;
 		PlayerFood--;
@@ -291,8 +291,8 @@ void game() {
 		break;
 	}
 	case 'd': {
-		if (GlobalEvent == 2) { cout << "You cannot escape from the monster to the right!" << endl; pause(); break; }
-		if (PlayerFood == 0) { cout << "You are hungry!" << endl; pause(); break; }
+		if (GlobalEvent == 2) { cout << "You cannot escape from the monster to the right!" << endl; IO::pause(); break; }
+		if (PlayerFood == 0) { cout << "You are hungry!" << endl; IO::pause(); break; }
 		PlayerX++;
 		Distancecovered++;
 		PlayerFood--;
@@ -312,12 +312,12 @@ void game() {
 			GlobalEvent = 0;
 			PlayerY--;
 			cout << "You cowardly ran away from the monster backward!" << endl;
-			saveGame();
-			pause();
+			SLgame::saveGame();
+			IO::pause();
 			break;
 		}
 		if (PlayerY > 0) {
-			if (PlayerFood == 0) { cout << "You are hungry!" << endl; pause(); break; }
+			if (PlayerFood == 0) { cout << "You are hungry!" << endl; IO::pause(); break; }
 			PlayerY--;
 			Distancecovered++;
 			PlayerFood--;
@@ -329,25 +329,25 @@ void game() {
 			PlayerFood++;
 			PlayerFoodInventory--;
 			cout << "You had a great snack!" << endl;
-			saveGame();
+			SLgame::saveGame();
 		}
 		else {
 			cout << "You cannot eat right now!" << endl;
 		}
-		pause();
+		IO::pause();
 		break;
 	}
 	case 'g': {
 		if (GlobalEvent != 2) {
 			cout << "There is no one to fight here!" << endl;
-			pause();
+			IO::pause();
 			break;
 		}
 		if (PlayerFood < 2) {
 			PlayerFood = 0;
 			cout << "You are too weak from hunger to fight! You lost..." << endl;
 			GlobalEvent = 0;
-			pause();
+			IO::pause();
 			break;
 		}
 
@@ -358,13 +358,13 @@ void game() {
 			KillMonsters++;
 			PlayerFoodInventory += 5;
 			GlobalEvent = 0;
-			saveGame();
+			SLgame::saveGame();
 		}
 		else {
 			PlayerFood -= 2;
 			cout << "Defeat! The monster mauled you pretty bad! Your hunger increased." << endl;
 		}
-		pause();
+		IO::pause();
 		break;
 	}
 	}
@@ -373,19 +373,19 @@ void game() {
 int main() {
 	srand(static_cast<unsigned int>(time(0)));
 
-	if (fileExists("save.txt")) {
+	if (Filesystem::fileExists("save.txt")) {
 		cout << "Old save file detected! Continue the game? [Y/N]" << endl;
 		char loadChoice = _getch();
 		loadChoice = tolower(loadChoice);
 		cout << endl;
 
 		if (loadChoice == 'y') {
-			loadGame();
+			SLgame::loadGame();
 			cout << "Game successfully loaded! Welcome back, " << PlayerName << "!" << endl;
 			Sleep(2000);
 
 			while (true) {
-				clearScreen();
+				IO::clearScreen();
 				game();
 			}
 			return 0;
@@ -394,13 +394,13 @@ int main() {
 
 	cout << "Hello, player, enter your name to begin" << endl;
 	getline(cin, PlayerName);
-	PlayerName = trim(PlayerName);
+	PlayerName = System::trim(PlayerName);
 	cout << "\nGreat, " << PlayerName << ", start the game?" << endl;
 	char choice = _getch();
 	choice = tolower(choice);
 	switch (choice) {
 	case 'y': {
-		clearScreen();
+		IO::clearScreen();
 		cout << "Starting!" << endl;
 		Sleep(2000);
 		break;
@@ -409,14 +409,14 @@ int main() {
 		exit(0);
 	}
 	default: {
-		clearScreen();
+		IO::clearScreen();
 		cout << "Starting!" << endl;
 		Sleep(2000);
 		break;
 	}
 	}
 	while (true) {
-		clearScreen();
+		IO::clearScreen();
 		game();
 	}
 	return 0;
--- HAR3D.cpp
+++ HAR3D.cpp
@@ -40,8 +40,8 @@ void startCombat() {
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleActiveScreenBuffer(hStdOut);
 
-	clearScreen();
-	setColor("4f");
+	IO::clearScreen();
+	IO::setColor("4f");
 	cout << "=========================================" << endl;
 	cout << "A fierce monster is in front of you! It blocks the way!" << endl;
 	cout << "=========================================" << endl;
@@ -59,7 +59,7 @@ void startCombat() {
 					PlayerFood = 0;
 					cout << "You are too weak from hunger to fight! You lost..." << endl;
 					GameMode = 0;
-					pause();
+					IO::pause();
 					break;
 				}
 
@@ -70,163 +70,180 @@ void startCombat() {
 					cout << "VICTORY! You defeated the monster! It dropped meat (+5 to inventory)" << endl;
 					PlayerFoodInventory += 5;
 					GameMode = 0;
-					saveGame();
+					SLgame::saveGame();
 				}
 				else {
 					PlayerFood -= 2;
 					cout << "Defeat! The monster mauled you pretty bad! Your hunger increased." << endl;
 				}
-				pause();
+				IO::pause();
 				break;
 			}
 			else if (combatChoice == 's') {
 				PlayerY -= 1.0f;
 				cout << "You cowardly ran away from the monster backward!" << endl;
 				GameMode = 0;
-				saveGame();
-				pause();
+				SLgame::saveGame();
+				IO::pause();
 				break;
 			}
 		}
 		Sleep(10);
 	}
-	setColor("1f");
-	render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16);
+	IO::setColor("1f");
+	Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16);
+}
+
+void game() {
+	try {
+		while (true) {
+			if (PlayerFood == 0 && PlayerFoodInventory == 0) {
+				HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleActiveScreenBuffer(hStdOut);
+
+				IO::setColor("4f");
+				IO::clearScreen();
+				cout << "=========================================" << endl;
+				cout << "  YOU DIED OF HUNGER IN THE FOREST...   " << endl;
+				cout << "=========================================" << endl;
+				cout << "Your final score: " << Distancecovered << " steps." << endl;
+				cout << "Monsters killed: " << KillMonsters << "." << endl;
+				cout << "Food found: " << FoundFood << "." << endl;
+				cout << "Kilometers covered: " << Distancecovered * 0.5 << " km." << endl;
+				IO::pause();
+				exit(0);
+			}
+
+
+			if (GameMode == 1) {
+				startCombat();
+				continue;
+			}
+
+			float oldX = PlayerX;
+			float oldY = PlayerY;
+
+			if (IO::isKeyPressed('A')) PlayerAngle3D -= 0.05f;
+			if (IO::isKeyPressed('D')) PlayerAngle3D += 0.05f;
+
+			if (IO::isKeyPressed('W')) {
+				int hungry = 0;
+				if (PlayerFood == 0) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); cout << "You are hungry!" << endl; IO::pause(); hungry = 1; Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
+				if (hungry == 0) {
+					float nextX = PlayerX + sinf(PlayerAngle3D) * 0.08f;
+					float nextY = PlayerY + cosf(PlayerAngle3D) * 0.08f;
+					if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '.' || ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '\0') {
+						PlayerX = nextX;
+						PlayerY = nextY;
+					}
+					Sleep(40);
+				}
+			}
+
+			if (IO::isKeyPressed('S')) {
+				int hungry = 0;
+				if (PlayerFood == 0) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); cout << "You are hungry!" << endl; IO::pause(); hungry = 1; Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
+				if (hungry == 0) {
+					float nextX = PlayerX - sinf(PlayerAngle3D) * 0.08f;
+					float nextY = PlayerY - cosf(PlayerAngle3D) * 0.08f;
+					if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '.' || ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] == '\0') {
+						PlayerX = nextX;
+						PlayerY = nextY;
+					}
+					Sleep(40);
+				}
+			}
+
+			if (IO::isKeyPressed('H')) {
+				if (PlayerFoodInventory > 0 && PlayerFood < 40) {
+					PlayerFood++;
+					PlayerFoodInventory--;
+					SLgame::saveGame();
+					Sleep(300);
+				}
+			}
+
+			if (IO::isKeyPressed(VK_ESCAPE)) {
+				SLgame::saveGame();
+				break;
+			}
+
+			if ((int)floorf(oldX) != (int)floorf(PlayerX) || (int)floorf(oldY) != (int)floorf(PlayerY)) {
+				Distancecovered++;
+				PlayerFood--;
+
+				int eventRoll = rand() % 9;
+				if (eventRoll < 2) {
+					PlayerFoodInventory++;
+					FoundFood++;
+					SLgame::saveGame();
+				}
+				else if (eventRoll == 4) {
+					GameMode = 1;
+				}
+
+
+				if (Distancecovered == 10 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Filesystem::Achievements("UNLOCKED: The first 10 kilometers!"); Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
+				if (Distancecovered == 100 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Filesystem::Achievements("UNLOCKED: The first 100 kilometers!"); PlayerFoodInventory += 6; Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
+				if (Distancecovered == 1000 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Filesystem::Achievements("UNLOCKED: The first 1000 kilometers!"); PlayerFoodInventory += 30; Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
+				if (Distancecovered == 40075 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Filesystem::Achievements("UNLOCKED: A trip around the world!"); PlayerFoodInventory = 200000; Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
+			}
+
+			Vector3D::render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16);
+			Sleep(15);
+		}
+	}
+	catch (...) {
+		HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleActiveScreenBuffer(hStdOut);
+
+		cout << "\n=========================================" << endl;
+		cout << "CRITICAL ERROR: ACCESS VIOLATION DETECTED!" << endl;
+		cout << "You stepped into a forbidden Windows memory zone." << endl;
+		cout << "Coordinates were unsafe. Emergency shutdown..." << endl;
+		cout << "=========================================" << endl;
+		IO::pause();
+		exit(1);
+	}
 }
 
 int main() {
 	srand(static_cast<unsigned int>(time(0)));
 
-	setColor("1f");
+	IO::setColor("1f");
 
-	if (fileExists("save.txt")) {
+	if (Filesystem::fileExists("save.txt")) {
 		cout << "Old save file detected! Continue the game? [Y/N]" << endl;
 		char loadChoice = _getch();
 		loadChoice = tolower(loadChoice);
 		if (loadChoice == 'y') {
-			loadGame();
+			SLgame::loadGame();
 			cout << "Game successfully loaded! Welcome back, " << PlayerName << "!" << endl;
 			Sleep(1500);
 		}
 		else {
 			cout << "Enter your name to begin a new journey:" << endl;
 			getline(cin, PlayerName);
-			PlayerName = trim(PlayerName);
+			PlayerName = System::trim(PlayerName);
 		}
 	}
 	else {
 		cout << "Hello, player, enter your name to begin:" << endl;
 		getline(cin, PlayerName);
-		PlayerName = trim(PlayerName);
+		PlayerName = System::trim(PlayerName);
 	}
 
 	if (Distancecovered == 0) {
-		clearScreen();
-		print_slow("-Hi! I found something...", 75);
+		IO::clearScreen();
+		IO::print_slow("-Hi! I found something...", 75);
 		Sleep(2500);
-		print_slow("-Hi, so what strange thing did you find?", 75);
+		IO::print_slow("-Hi, so what strange thing did you find?", 75);
 		Sleep(2500);
-		print_slow("-It... looks like some kind of creature!", 75);
+		IO::print_slow("-It... looks like some kind of creature!", 75);
 		Sleep(2500);
-		print_slow("-Hahaha, I don't believe you, so I'm heading into that forest myself!", 75);
-		pause();
+		IO::print_slow("-Hahaha, I don't believe you, so I'm heading into that forest myself!", 75);
+		IO::pause();
 	}
-
-	while (true) {
-		if (PlayerFood == 0 && PlayerFoodInventory == 0) {
-			HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-			SetConsoleActiveScreenBuffer(hStdOut);
-
-			setColor("4f");
-			clearScreen();
-			cout << "=========================================" << endl;
-			cout << "  YOU DIED OF HUNGER IN THE FOREST...   " << endl;
-			cout << "=========================================" << endl;
-			cout << "Your final score: " << Distancecovered << " steps." << endl;
-			cout << "Monsters killed: " << KillMonsters << "." << endl;
-			cout << "Food found: " << FoundFood << "." << endl;
-			cout << "Kilometers covered: " << Distancecovered * 0.5 << " km." << endl;
-			pause();
-			exit(0);
-		}
-
-
-		if (GameMode == 1) {
-			startCombat();
-			continue;
-		}
-
-		float oldX = PlayerX;
-		float oldY = PlayerY;
-
-		if (isKeyPressed('A')) PlayerAngle3D -= 0.05f;
-		if (isKeyPressed('D')) PlayerAngle3D += 0.05f;
-
-		if (isKeyPressed('W')) {
-			int hungry = 0;
-			if (PlayerFood == 0) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); cout << "You are hungry!" << endl; pause(); hungry = 1; render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
-			if (hungry == 0) {
-				float nextX = PlayerX + sinf(PlayerAngle3D) * 0.08f;
-				float nextY = PlayerY + cosf(PlayerAngle3D) * 0.08f;
-				if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] != '#') {
-					PlayerX = nextX;
-					PlayerY = nextY;
-				}
-				Sleep(40);
-			}
-		}
-
-		if (isKeyPressed('S')) {
-			int hungry = 0;
-			if (PlayerFood == 0) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); cout << "You are hungry!" << endl; pause(); hungry = 1; render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
-			if (hungry == 0) {
-				float nextX = PlayerX - sinf(PlayerAngle3D) * 0.08f;
-				float nextY = PlayerY - cosf(PlayerAngle3D) * 0.08f;
-				if (ForestMap3D[(int)floorf(nextY) * 16 + (int)floorf(nextX)] != '#') {
-					PlayerX = nextX;
-					PlayerY = nextY;
-				}
-				Sleep(40);
-			}
-		}
-
-		if (isKeyPressed('H')) {
-			if (PlayerFoodInventory > 0 && PlayerFood < 40) {
-				PlayerFood++;
-				PlayerFoodInventory--;
-				saveGame();
-				Sleep(300);
-			}
-		}
-
-		if (isKeyPressed(VK_ESCAPE)) {
-			saveGame();
-			break;
-		}
-
-		if ((int)floorf(oldX) != (int)floorf(PlayerX) || (int)floorf(oldY) != (int)floorf(PlayerY)) {
-			Distancecovered++;
-			PlayerFood--;
-
-			int eventRoll = rand() % 9;
-			if (eventRoll < 2) {
-				PlayerFoodInventory++;
-				FoundFood++;
-				saveGame();
-			}
-			else if (eventRoll == 4) {
-				GameMode = 1;
-			}
-
-
-			if (Distancecovered == 10 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Achievements("UNLOCKED: The first 10 kilometers!"); render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
-			if (Distancecovered == 100 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Achievements("UNLOCKED: The first 100 kilometers!"); PlayerFoodInventory += 6; render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
-			if (Distancecovered == 1000 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Achievements("UNLOCKED: The first 1000 kilometers!"); PlayerFoodInventory += 30; render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
-			if (Distancecovered == 40075 * 2) { HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleActiveScreenBuffer(hStdOut); Achievements("UNLOCKED: A trip around the world!"); PlayerFoodInventory = 200000; render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16); }
-		}
-
-		render3D_Ultimate(PlayerAngle3D, ForestMap3D, 16, 16);
-		Sleep(15);
-	}
+	game();
 	return 0;
 }
